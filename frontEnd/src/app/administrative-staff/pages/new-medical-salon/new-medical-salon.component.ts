9378bbcef600a97191d7154f6c18939f09ebed52
Kun Kin Zheng Liang <kzheng@estudiantec.cr>
2024-06-05 01:35:31 -0600

    [ADD] salon form html

--- frontEnd/src/app/administrative-staff/pages/new-medical-salon/new-medical-salon.component.ts
+++ frontEnd/src/app/administrative-staff/pages/new-medical-salon/new-medical-salon.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SalonService } from '../../services/salon.service';
+import { Salon } from '../../interfaces/salon.interface';
 
 @Component({
   selector: 'app-new-medical-salon',
@@ -6,5 +12,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-medical-salon.component.css']
 })
 export class NewMedicalSalonComponent {
+
+  constructor(
+    private fb: FormBuilder,
+    private salonService: SalonService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog
+ ) { }
+
+  public salonForm = this.fb.group({
+    number: ['', Validators.required],
+    name: ['', Validators.required],
+    capacity: ['', Validators.required],
+    type: ['', Validators.required],
+    floor: ['', Validators.required],
+  });
+
+  public types = [
+    { id: 'w', desc: 'Mujeres' },
+    { id: 'm', desc: 'Hombres' },
+    { id: 'c', desc: 'Niños' }
+  ];
+
+  currentSalonToUpdate: Boolean = false;
+
+  get currentSalon(): Salon {
+    const salon = this.salonForm.value as unknown as Salon;
+
+    return salon;
+  }
+
+  onSubmit() {
+
+  }
+
+  onDeleteSalon() {
+
+  }
+
+  showSnackbar(message: string): void {
+    this.snackbar.open(message, 'Entendido', {
+      duration: 2500
+    });
+  }
 
 }
--- frontEnd/src/app/administrative-staff/services/salon.service.ts
+++ frontEnd/src/app/administrative-staff/services/salon.service.ts
@@ -20,4 +20,28 @@ export class SalonService {
 
     return this.http.get<Salon[]>(url);
   }
+
+  getSalonById(id: number): Observable<Salon> {
+    const url = `${this.baseUrl}/salon/${id}`;
+
+    return this.http.get<Salon>(url);
+  }
+
+  createSalon(salon: Salon): Observable<Salon> {
+    const url = `${this.baseUrl}/salon`;
+
+    return this.http.post<Salon>(url, salon);
+  }
+
+  updateSalon(salon: Salon): Observable<Salon> {
+    const url = `${this.baseUrl}/salon/${salon.number}`;
+
+    return this.http.put<Salon>(url, salon);
+  }
+
+  deleteSalon(id: number): Observable<Salon> {
+    const url = `${this.baseUrl}/salon/${id}`;
+
+    return this.http.delete<Salon>(url);
+  }
 }
