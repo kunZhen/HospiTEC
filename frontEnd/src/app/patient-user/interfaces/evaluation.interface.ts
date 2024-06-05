8dec8e46edfe1ab0c02539e8eceb89d92c07adf1
Kun Kin Zheng Liang <kzheng@estudiantec.cr>
2024-06-04 22:50:53 -0600

    [ADD] evaluation service

---
+++ frontEnd/src/app/patient-user/interfaces/evaluation.interface.ts
@@ -0,0 +1,9 @@
+export interface Evaluation {
+  recordId: string;
+  cleanlinessComment: string;
+  cleanlinessRate: number;
+  staffTreatmentComment: string;
+  staffTreatmentRate: number;
+  appointmentPunctualityComment: string;
+  appointmentPunctualityRate: number;
+}
--- frontEnd/src/app/patient-user/pages/grade-page/grade-page.component.ts
+++ frontEnd/src/app/patient-user/pages/grade-page/grade-page.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Record } from 'src/app/patient/interfaces/record.interface';
 import { RecordService } from '../../../patient/services/record.service';
 import { delay, switchMap } from 'rxjs';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EvaluationService } from '../../services/evaluation.service';
+import { Evaluation } from '../../interfaces/evaluation.interface';
 
 @Component({
   selector: 'app-grade-page',
@@ -10,6 +14,8 @@ import { delay, switchMap } from 'rxjs';
   styleUrls: ['./grade-page.component.css']
 })
 export class GradePageComponent implements OnInit {
+
+  public recordId: string = '';
 
   public record?: Record;
   public maxRating: number = 5;
@@ -26,11 +32,25 @@ export class GradePageComponent implements OnInit {
   public appoinmentPunctualitySelectedStar: number = 0;
   public appoinmentPunctualityPreviousSelection: number = 0;
 
+  public evaluationForm = this.fb.group({
+    cleanliness: [0, [Validators.required]],
+    staffTreatment: [0, [Validators.required]],
+    appoinmentPunctuality: [0, [Validators.required]],
+    commentsCleanliness: ['', Validators.required],
+    commentsStaffTreatment: ['', Validators.required],
+    commentsAppoinmentPunctuality: ['', Validators.required]
+  });
+
   constructor(
     private recordService: RecordService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder,
+    private snackbar: MatSnackBar,
+    private evaluationService: EvaluationService
+
   ) {}
+
 
   ngOnInit(): void {
     this.activatedRoute.params
@@ -43,6 +63,7 @@ export class GradePageComponent implements OnInit {
         if (!record) return this.router.navigate([ '/patient/list' ]);
 
         this.record = record;
+        this.recordId = record.id;
         console.log(this.record);
         return;
       })
@@ -54,6 +75,44 @@ export class GradePageComponent implements OnInit {
 
   goBack():void {
     this.router.navigateByUrl('patient-user/records')
+  }
+
+  onSubmit() {
+    if (this.evaluationForm.invalid) return;
+
+    const { cleanliness, staffTreatment, appoinmentPunctuality, commentsCleanliness, commentsStaffTreatment, commentsAppoinmentPunctuality } = this.evaluationForm.value;
+
+    const evaluation: Evaluation = {
+      recordId: this.recordId,
+      cleanlinessComment: commentsCleanliness!,
+      cleanlinessRate: cleanliness!,
+      staffTreatmentComment: commentsStaffTreatment!,
+      staffTreatmentRate: staffTreatment!,
+      appointmentPunctualityComment: commentsAppoinmentPunctuality!,
+      appointmentPunctualityRate: appoinmentPunctuality!
+    }
+
+    this.evaluationService.existEvaluation(evaluation.recordId).subscribe( exist => {
+      if (exist) {
+        this.evaluationService.updateEvaluation(evaluation).subscribe( () => {
+          this.showSnackbar('Evaluación actualizada correctamente');
+          this.router.navigateByUrl('patient-user/records');
+        });
+      } else {
+        this.evaluationService.addEvaluation(evaluation).subscribe( () => {
+          this.showSnackbar('Evaluación añadida correctamente');
+          this.router.navigateByUrl('patient-user/records');
+        });
+      }
+    });
+
+    console.log(evaluation);
+  }
+
+  showSnackbar(message: string): void {
+    this.snackbar.open(message, 'Entendido', {
+      duration: 2500
+    });
   }
 
   // Cleanliness
@@ -70,7 +129,7 @@ export class GradePageComponent implements OnInit {
   rating(index: number) {
     this.cleanlinessSelectedStar = index + 1;
     this.previousSelection = this.cleanlinessSelectedStar;
-    console.log(this.cleanlinessSelectedStar);
+    this.evaluationForm.patchValue({ cleanliness: this.cleanlinessSelectedStar });
   }
 
   // Staff Treatment
@@ -87,7 +146,7 @@ export class GradePageComponent implements OnInit {
   rateStaffTreatment(index: number) {
     this.staffTreatmentSelectedStar = index + 1;
     this.staffTreatmentPreviousSelection = this.staffTreatmentSelectedStar;
-    console.log(this.staffTreatmentSelectedStar);
+    this.evaluationForm.patchValue({ staffTreatment: this.staffTreatmentSelectedStar });
   }
 
   // Appoinment Punctuality
@@ -104,6 +163,6 @@ export class GradePageComponent implements OnInit {
   rateAppoinmentPunctuality(index: number) {
     this.appoinmentPunctualitySelectedStar = index + 1;
     this.appoinmentPunctualityPreviousSelection = this.appoinmentPunctualitySelectedStar;
-    console.log(this.appoinmentPunctualitySelectedStar);
+    this.evaluationForm.patchValue({ appoinmentPunctuality: this.appoinmentPunctualitySelectedStar });
   }
 }
